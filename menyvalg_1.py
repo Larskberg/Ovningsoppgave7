79df7b168f3045c471fbef82bedd40f6328de747
David <angdrom@gmail.com>
2025-10-13 16:44:05 +0200

    Add files via upload

---
+++ menyvalg_1.py
@@ -0,0 +1,11 @@
+from emner import emnekoder, semestre, studiepoeng
+
+def kjør():
+    print("\n--- Legg til nytt emne ---")
+    kode = input("Emnekode: ")
+    sem = input("Semester (høst/vår): ").lower()
+    sp = int(input("Studiepoeng: "))
+    emnekoder.append(kode)
+    semestre.append(sem)
+    studiepoeng.append(sp)
+    print(f"Emnet {kode} ({sem}, {sp} sp) er registrert.")

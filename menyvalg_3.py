c30302b5344e14b51536ea6118741cf89f261775
David <angdrom@gmail.com>
2025-10-14 13:48:35 +0200

    Update menyvalg_3.py

--- menyvalg_3.py
+++ menyvalg_3.py
@@ -1,1 +1,8 @@
+from emner import emnekoder, semestre, studiepoeng
 
+def kjør():
+    print("\nRegistrerte emner:")
+    if not emnekoder:
+        print("Ingen emner registrert ennå.")
+    for i, kode in enumerate(emnekoder):
+        print(f"{i}: {kode} ({semestre[i]}, {studiepoeng[i]} sp)")

2aecb3b53c9f452b4403343a7c99e9646a39bca0
David <angdrom@gmail.com>
2025-10-15 09:26:37 +0200

    Update menyvalg_2.py

--- menyvalg_2.py
+++ menyvalg_2.py
@@ -1,1 +1,7 @@
+from studieplan import legg_til_emne
 
+def kjør():
+    print("\n--- Legg til emne i studieplan ---")
+    emne_index = int(input("Indeks på emne: "))
+    sem_nr = int(input("Semester (1-6): "))
+    legg_til_emne(emne_index, sem_nr)

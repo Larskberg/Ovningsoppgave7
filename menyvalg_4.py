c74f5ab4e53dfb5491675f8dbeaf6b127a8a1b4f
David <angdrom@gmail.com>
2025-10-15 09:30:08 +0200

    Update menyvalg_4.py

--- menyvalg_4.py
+++ menyvalg_4.py
@@ -1,1 +1,9 @@
+from emner import emnekoder, studiepoeng
+from studieplan import hent_studieplan
 
+def kjør():
+    print("\nStudieplan:")
+    for i, sem in enumerate(hent_studieplan(), start=1):
+        total_sp = sum(studiepoeng[j] for j in sem)
+        emner = [emnekoder[j] for j in sem]
+        print(f"Semester {i}: {emner} ({total_sp} sp)")

a3db8038aac9db9ef87d70dc2c4100b2da8f4a39
David <angdrom@gmail.com>
2025-10-16 08:45:59 +0200

    Fix formatting and comments in studieplan.py

--- studieplan.py
+++ studieplan.py
@@ -1,6 +1,5 @@
 # Hoved fil som håndterer studieplanen og alle gyldighetssjekkene som må gjørs.
 # Hver sjekk har kommentar over for lesbarhet.
-
 from emner import emnekoder, semestre, studiepoeng
 
 studieplan = [[] for _ in range(6)]
@@ -43,5 +42,6 @@ def legg_til_emne(emne_index, semester_nr):
 
 def hent_studieplan():
     return studieplan
+
 
 

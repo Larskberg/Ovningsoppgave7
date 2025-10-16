2902b1b777d9b83a5dcf7cfe3c5e92812b378a84
Larskberg <02laber@gmail.com>
2025-10-16 09:14:13 +0200

    Update menyvalg_6.py

--- menyvalg_6.py
+++ menyvalg_6.py
@@ -4,22 +4,27 @@ from studieplan import hent_studieplan
 
 
 def studieplanfil():
-    filnavn = 'studieplan.txt'
-    studieplan = hent_studieplan()
-    with open(filnavn, "w") as fila:
-        for sem_nr, sem in enumerate(studieplan, start=1):
-            fila.write(f"{sem_nr}. semester:\n")
-            for i in sem:
-                fila.write(
-                    f"{emnekoder[i]}, {studiepoeng[i]}, {semestre[i]}\n"
-                )
-    print("Laget fil med studieplan")
-
+    try:
+        filnavn = 'studieplan.txt'
+        studieplan = hent_studieplan()
+        with open(filnavn, "w") as fila:
+            for sem_nr, sem in enumerate(studieplan, start=1):
+                fila.write(f"{sem_nr}. semester:\n")
+                for i in sem:
+                    fila.write(
+                        f"{emnekoder[i]}, {studiepoeng[i]}, {semestre[i]}\n"
+                    )
+        print("Laget fil med studieplan")
+    except TypeError:
+        print('Kan ikkke lage studieplanfil')
 
 def emnefil():
-    filnavn = 'emner.txt'
-    with open(filnavn, 'w') as fila:
-        for i, kode in enumerate(emnekoder):
-            fila.write(f"{kode}, {studiepoeng[i]}, {semestre[i]}\n")
-    print("Laget fil med emner")
+    try:
+        filnavn = 'emner.txt'
+        with open(filnavn, 'w') as fila:
+            for i, kode in enumerate(emnekoder):
+                fila.write(f"{kode}, {studiepoeng[i]}, {semestre[i]}\n")
+        print("Laget fil med emner")
+    except TypeError:
+        print('Kan ikke lage emnefil')
 

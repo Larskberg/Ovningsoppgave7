a70bff1aa6284d6b1a9e79c0c8239502689995d7
David <angdrom@gmail.com>
2025-10-16 09:21:57 +0200

    Legger in sjekk i lag nytt emne
    sjekker at inputtene er riktig

--- menyvalg_1.py
+++ menyvalg_1.py
@@ -3,13 +3,26 @@ from emner import emnekoder, semestre, studiepoeng
 
 
 def lag_nytt_emne():
-    print("\n--- Legg til nytt emne ---")
-    kode = input("Emnekode: ")
-    sem = input("Semester (høst/vår): ").lower()
-    sp = int(input("Studiepoeng: "))
-    emnekoder.append(kode)
-    semestre.append(sem)
-    studiepoeng.append(sp)
-    print(f"Emnet {kode} ({sem}, {sp} sp) er registrert.")
+    try:
+        print("\n--- Legg til nytt emne ---")
+        kode = input("Emnekode: ")
 
+        # Valider semester
+        while True:
+            sem = input("Semester (høst/vår): ").lower()
+            if sem in ("høst", "vår"):
+                break
+            else:
+                print("Ugyldig valg. Du må skrive 'høst' eller 'vår'.")
 
+        # Valider studiepoeng
+        sp = int(input("Studiepoeng: "))
+
+        emnekoder.append(kode)
+        semestre.append(sem)
+        studiepoeng.append(sp)
+        print(f"Emnet {kode} ({sem}, {sp} sp) er registrert.")
+
+    except ValueError:
+        print("Ugyldig input. Studiepoeng må være et tall.")
+        

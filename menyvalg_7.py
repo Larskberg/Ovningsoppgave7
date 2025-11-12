d843a2ad1afd5340286bdd8e71f8f26f26c36618
David <angdrom@gmail.com>
2025-11-12 14:02:17 +0100

    Skal oppdateres med ny funksjon

--- menyvalg_7.py
+++ menyvalg_7.py
@@ -1,17 +1,1 @@
-# Leser opp emner fra txt fil lagret fra menyvalg 6
-def les_fra_fil():
-    with open('studieplan.txt', 'r') as fila:
-        print('Studieplan:')
-        for linje in fila:
-            print(linje + '\n')
-            
-            
-            
-    with open('emner.txt', 'r') as fila:
-        print('Emner:')
-        for linje in fila:
-            print(linje + '\n')
 
-    
-
-            

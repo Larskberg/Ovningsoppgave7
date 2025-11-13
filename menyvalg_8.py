770d02834038d1979821bbeda5644d4eb8e297e0
Larskberg <02laber@gmail.com>
2025-11-13 10:40:37 +0100

    Add files via upload

--- menyvalg_8.py
+++ menyvalg_8.py
@@ -3,11 +3,19 @@ from emner import emner
 from menyvalg_5 import studieplaner, Studieplan
 
 def finn_studieplaner_med_emne():
+    print('Emner:')
+    for emne in emner:
+        print(f' - {emne}')
     sjekk = input('Hvilket emne vil du sjekke?')
+    if sjekk in emner[1]:
+        
+        for key in studieplaner:
+            if any(sjekk in emneliste for emneliste in key.semestre.values()):
+                     print(studieplaner[key])
+            else:
+                print('Emne finnes ikke i noen studieplan')
+                    
     if sjekk not in emner:
         print('Emne er ikke registrert')
         
-    else:
-        for key in studieplaner:
-             if any(sjekk in emneliste for emneliste in sjekk.semestre.values()):
-                 print(studieplaner[key])
+   
